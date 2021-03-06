95596190ddf3afe11fbd4f6d1c6020b5c358b7b8
James Kernan <jtkernan92@gmail.com>
2021-03-06 12:40:39 -0700

    deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DB_URL || 'postgresql://jamesk_projects@localhost/runwithit',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://jamesk_projects@localhost/runwithit',
   }
